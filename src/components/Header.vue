550a0c747272f9e6c0c7a0d5f061b3502e0ea695
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-17 15:06:06 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="container-fluid">
+		<img class="py-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+	</div>
+  
+</template>
+
+<script>
+export default {
+	name: "Header",
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+.container-fluid {
+	background-color: $headerBackground;
+	img {
+		height: 55px;
+	}
+}
+
+</style>
