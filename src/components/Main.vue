ce60418f8562a98b31a6703090c8e5dc292f738c
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-17 15:20:20 +0100

    static layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,84 @@
 <template>
-  <div class="container-70 my-5">
+  <div class="container-60 my-5">
 		<div class="row row-cols-5">
 			<div class="col">
 				<div class="spotify-card">
-					<img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
-					<h2>new jersy</h2>
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
+				</div>
+			</div>
+			<div class="col">
+				<div class="spotify-card">
+					<img class="p-3" src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+					<span class="title">new jersy</span>
+					<span class="subtitle">Bon Jovi</span>
+					<span class="year">1988</span>
 				</div>
 			</div>
 		</div>
@@ -22,9 +96,12 @@ export default {
 
 <style lang="scss">
 @import "../assets/scss/style.scss";
-.container-70 {
-	width: 70%;
+.container-60 {
+	width: 60%;
 	margin: auto;
+	.col {
+		padding: 0.5em 1em;
+	}
 	.spotify-card {
 		background-color: $headerBackground;
 		display: flex;
@@ -33,8 +110,13 @@ export default {
 		img {
 			width: 100%;
 		}
-		h2 {
+		.title {
 			text-transform: uppercase;
+			color: white;
+			font-weight: 500;
+		}
+		.subtitle, .year {
+			color: $spanColor;
 		}
 	}
 }
